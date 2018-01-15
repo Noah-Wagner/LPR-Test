b8ec1927a99d84a8c0d1a7012545e8a3280d1cb8
Noah-Wagner <noah@noah-wagner.com>
2018-01-15 12:05:33 -0500

    Reverted change

--- batch_crop.py
+++ batch_crop.py
@@ -23,3 +23,5 @@ for folder_name in os.listdir(directory):
             image = Image.open(image_file_path)
             image = crop_image(image)
             image.save(image_file_path)
+
+print("Done")
