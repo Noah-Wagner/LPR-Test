018c14d401e4cf9dd19d8992eadf58e66c5be011
Noah-Wagner <noah@noah-wagner.com>
2018-01-16 14:49:55 -0500

    Removed debug statements

--- build_manifest.py
+++ build_manifest.py
@@ -1,6 +1,8 @@
 import os
 import pandas
 
+import validate_file_name
+
 file = r"20151124-075524_FXR8668.jpg"
 
 base_directory = r"N:\User\NAWagner\LPR\Curated"
@@ -14,17 +16,16 @@ assert get_plate(file) == 'FXR8668'
 
 directory = os.fsencode(base_directory)
 
-file_names = []
+image_list = validate_file_name.get_image_list()
+
 license_plates = []
 
-for file in os.listdir(directory):
-    file_name = os.fsdecode(file)
-    if file_name.endswith(".jpg"):
-        file_names.append(file_name)
-        license_plates.append(get_plate(file_name))
+for image in image_list:
+    image_name = os.path.basename(image)
+    license_plates.append(get_plate(image_name))
 
 writer = pandas.ExcelWriter(os.path.join(base_directory, "manifest.xlsx"))
-data_frame = pandas.DataFrame({'Filenames': file_names, 'LP': license_plates})
+data_frame = pandas.DataFrame({'LP': license_plates, 'Path': image_list})
 data_frame.to_excel(writer, index=False)
 writer.save()
 
